dba55969693a67da0d2ecbc0e047aba4f1a19acc
Eliezer solano <develiezermartinez@gmail.com>
2026-01-23 13:14:10 -0600

    refactor: :wrench: Refactor para config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // O 'hybrid'
-  adapter: cloudflare({
-    imageService: "compile", // <--- AGREGA ESTO
-  }),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
